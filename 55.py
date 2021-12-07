1336794b6a0e903b1e26d4b88a3e5a0b9f08bc37
rothsa <rothsa@users.noreply.github.com>
2021-12-07 07:19:37 +0100

    Create 55.py

---
+++ 55.py
@@ -0,0 +1,17 @@
+#Write your code below this row 👇
+
+
+#Your program should print each number from 1 to 100 in turn.
+#When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+#When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
+#And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
+
+for n in range(1, 101):
+    if n % 15 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else: 
+        print(n)       
