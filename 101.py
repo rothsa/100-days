e1a888e41afd1258c1d78a3b8ab5073a18714978
rothsa <rothsa@users.noreply.github.com>
2022-01-02 10:16:54 +0100

    Create 101.py

---
+++ 101.py
@@ -0,0 +1,37 @@
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+def days_in_month(year_entry, month_entry):
+    month_days = {1: 31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    if is_leap(year_entry) and  month_entry == 2:
+        month_days[2] = 29
+    return month_days[month_entry]
+  
+  
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+
+
+
+
+
+
+
+
+
+
+
